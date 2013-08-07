a9da30aeead43ddd4f21a1d033a9e76e1e164a96
bethanyr <bethany.rentz@hotmail.com>
2013-08-07 13:55:16 -0700

    initial version of application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Helpdesk::Application.config.secret_token = '23c2acbeffcc2f49cda198281199fee252cd3f8c37c1c99e042d00706f87b4d8cbbf5d9700a46cf6d44cb8edc0aa595877361aa37a1c8271a8a0254d5ef42422'
