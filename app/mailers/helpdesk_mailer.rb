32ec2289cacad2ab9fe9c61499b61ad22fe0c4ff
bethanyr <bethany.rentz@hotmail.com>
2013-08-08 10:30:00 -0700

    updated reply_to field

--- app/mailers/helpdesk_mailer.rb
+++ app/mailers/helpdesk_mailer.rb
@@ -8,6 +8,6 @@ class HelpdeskMailer < ActionMailer::Base
   def ticket_confirmation(ticket)
     @ticket = ticket
     @mail_to = "bethanynrentz@gmail.com"
-    mail to: @mail_to, subject: @ticket.id.to_s + "-" + @ticket.name.to_s, from: "bethany.helpdesk@gmail.com"
+    mail to: @mail_to, subject: @ticket.id.to_s + "-" + @ticket.name.to_s, from: "bethany.helpdesk@gmail.com", reply_to: "4cbcd1717700ab15df8c@cloudmailin.net"
   end
 end
